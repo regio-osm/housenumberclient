b4a9a44963a2466f5ee2bdc1152dea5e5e7301f9
okilimu <githubb@diesei.de>
2015-04-07 12:19:00 +0200

    add local housenumberlist file support and enhance program call parameter inline doc

--- src/Evaluation.java
+++ src/Evaluation.java
@@ -154,6 +154,22 @@ public class Evaluation {
 			System.out.println("args[" + lfdnr + "] ===" + args[lfdnr] + "===");
 		}
 		if ((args.length >= 1) && (args[0].equals("-h"))) {
+			System.out.println("German help available with -hDE");
+			System.out.println("");
+			System.out.println("For local housnumberlist evaluations MUST parameters:");
+			System.out.println("-file importfilename");
+			System.out.println("-relationid: OSM-Relationid for Municipality, for example 123456");
+			System.out.println("");
+			System.out.println("For local housenumberlist evaluations OPTIONAL parameters:");
+			System.out.println("-columnseparator: Character for separation of file line columns (Default, if unset: TAB-Character)");
+			System.out.println("-housenumbercasesensity: should alphabetical suffixes at housenumbers be case sensitive ('yes') or not (default 'no')");
+			System.out.println("");
+			System.out.println("For local housenumberlist evaluations Information about the local housenumberlist:");
+			System.out.println("The import file must have a first comment line, starting with the hash character #");
+			System.out.println("In this comment line, the column titles must be set in any order: Stadt	Straße   Hausnummer    Hausnummerzusatz   Hausnummerzusatz2   Subid");
+			System.out.println("Furthermore column titles will be ignored");
+			System.out.println("");
+			System.out.println("Help for client-server mode and its program call parameters:");
 			System.out.println("-country countryname -- ('Bundesrepublik Deutschland' or other countries in english version, for exampel 'Netherland'");
 			System.out.println("-adminhierarchy xy -- administrative hierarchy, for example 'Bundesrepublik Deutschland,Bayern'");
 			System.out.println("-municipality muniname -- name of the municipality");
@@ -164,7 +180,33 @@ public class Evaluation {
 			System.out.println("-maxminutes 30 -- maximum number of minutes, the program should run. A running evaluation will be finished.");
 			return;
 		}
-		String parameterCountry = "";
+		if ((args.length >= 1) && (args[0].equals("-hDE"))) {
+			System.out.println("Für lokale Auswertungen PFLICHT-Parameter:");
+			System.out.println("-file importdateiname");
+			System.out.println("-relationid: OSM-Relationsid für die Gemeinde, z.B. 123456");
+			System.out.println("");
+			System.out.println("Für lokale Auswertungen WAHL-Parameter:");
+			System.out.println("-columnseparator: Zeichen zur Trennung der Spalten in einer Dateizeile (Standard, wenn ungesetzt: TAB-Zeichen)");
+			System.out.println("-housenumbercasesensity: Angabe, ob alphabetische Hausnummerzusätze groß-/kleinschreibrelevant ausgewertet werden sollen (Standard: nein), Werte ja, nein");
+			System.out.println("");
+			System.out.println("Für lokale Auswertungen Angaben zur Hausnummerdatei:");
+			System.out.println("Importdatei muss in der ersten Zeile eine Kommentarzeile haben, die mit # beginnt");
+			System.out.println("Mögliche Spaltentitel sind in beliebiger Reihenfolge:   Stadt	Straße   Hausnummer    Hausnummerzusatz   Hausnummerzusatz2   Subid");
+			System.out.println("Weitere Spaltentitel können vorhanden sein und werden ignoriert");
+			System.out.println("");
+			System.out.println("Für Client/Server Auswertungen PFLICHT-Parameter:");
+			System.out.println("-country: 'Bundesrepublik Deutschland' wenn fehlend");
+			System.out.println("-municipality: Stadtname");
+			System.out.println("-officialkeysid 8stelligeramtlicherGemeindeschlüssel z.B. 09761000");
+			System.out.println("-adminhierarchy xy -- administrative hierarchy, for example 'Bundesrepublik Deutschland,Bayern'");
+			System.out.println("-jobname xy -- jobname of the municipality. Name of the muncipality or of a subadmin area, if available in osm and in offizial housenumber list");
+			System.out.println("-allmissingjobs -- flag, that all missing jobs in a country should be run (default: only as specified in other parameters");
+			System.out.println("-maxjobs 4711 -- maximum number of jobs, that should be worked on");
+			System.out.println("-maxminutes 30 -- maximum number of minutes, the program should run. A running evaluation will be finished.");
+			return;
+		}
+
+		String parameterCountry = "Bundesrepublik Deutschland";
 		String parameterAdminHierarchy = "";
 		String parameterMunicipiality = "";
 		String parameterJobname = "";
@@ -172,6 +214,11 @@ public class Evaluation {
 		boolean parameterAllMissingJobs = false;
 		int parameterMaxJobs = -1;
 		int parameterMaxMinutes = -1;
+		boolean parameterHousenumbersCaseSensity = false;
+			// client call parameters
+		String parameterImportdateiname = "";
+		String parameterFieldSeparator = "\t";
+		Integer parameterOSMRelationid = 0;
 		
 		if (args.length >= 1) {
 			int argsOkCount = 0;
@@ -200,7 +247,6 @@ public class Evaluation {
 					parameterOfficialkeysId = args[argsi + 1];
 					argsOkCount  += 2;
 				}
-
 				if (args[argsi].equals("-maxjobs")) {
 					parameterMaxJobs = Integer.parseInt(args[argsi + 1]);
 					argsOkCount  += 2;
@@ -213,9 +259,28 @@ public class Evaluation {
 					parameterAllMissingJobs = true;
 					argsOkCount  += 1;
 				}
+				if (args[argsi].equals("-file")) {
+					parameterImportdateiname = args[argsi + 1];
+					argsOkCount += 2;
+				}
+				if (args[argsi].equals("-housenumbercasesensity")) {
+					String yesno = args[argsi + 1].toLowerCase().substring(0,1);
+					if (yesno.equals("y") || yesno.equals("j")) {
+						parameterHousenumbersCaseSensity = true;
+					} else {
+						parameterHousenumbersCaseSensity = false;
+					}
+					argsOkCount += 2;
+				}
 
-				System.out.println("-maxjobs 4711 -- maximum number of jobs, that should be worked on");
-System.out.println("-maxminutes 30 -- maximum number of minutes, the program should run. A running evaluation will be finished.");
+				if (args[argsi].equals("-relationid")) {
+					parameterOSMRelationid = Integer.parseInt(args[argsi + 1]);
+					argsOkCount += 2;
+				}
+				if (args[argsi].equals("-columnseparator")) {
+					parameterFieldSeparator = args[argsi + 1];
+					argsOkCount += 2;
+				}
 			}
 			if (argsOkCount < args.length) {
 				System.out.println("ERROR: not all programm parameters were valid, STOP");
@@ -230,118 +295,140 @@ System.out.println("-maxminutes 30 -- maximum number of minutes, the program sho
 			System.out.println("Parameter -country must be set at least and can't contain a wildcard");
 			return;
 		}
-		
-		
+
 		parameterAdminHierarchy = parameterAdminHierarchy.replace("*", "%");
 		parameterMunicipiality = parameterMunicipiality.replace("*", "%");
 		parameterJobname = parameterJobname.replace("*", "%");
 		parameterOfficialkeysId = parameterOfficialkeysId.replace("*", "%");
 
-		
-		
 		HousenumberlistReader hnrreader = new HousenumberlistReader();
-		hnrreader.setDBConnection(	configuration.db_application_url, 
-									configuration.db_application_username, 
-									configuration.db_application_password);
 		OsmDataReader osmreader = new OsmDataReader();
 		Evaluation evaluation = new Evaluation();
-		HousenumberServerAPI hnrserver = new HousenumberServerAPI();
+
+		if(! parameterCountry.equals("") && (! parameterMunicipiality.equals("")))
+			evaluation.setMunicipality(parameterCountry, parameterMunicipiality);
+		evaluation.setHousenumberAdditionCaseSensity(parameterHousenumbersCaseSensity);
+		System.out.println("Number of housenumberlist entries at start: " + evaluation.housenumberlist.length());
 
 		HousenumberCollection list_housenumbers = new HousenumberCollection();
 		HousenumberCollection osm_housenumbers = new HousenumberCollection();
 		HousenumberCollection evaluated_housenumbers = new HousenumberCollection();
 		
-		List<Job> jobs;
-
-		if(parameterAllMissingJobs)
-			jobs = hnrserver.getMissingCountryJobs(parameterCountry);
-		else
-			jobs = hnrserver.findJobs(parameterCountry,parameterMunicipiality, parameterJobname, parameterOfficialkeysId);
-		
-
-		//jobs = hnrserver.findJobs("Schweiz","Zürich", "Zürich", "*");
-		
-		System.out.println("Number of Jobs received from Server: " + jobs.size());
-		
-		java.util.Date jobstart = new java.util.Date();
-		java.util.Date jobend = new java.util.Date();
-
-		for(int jobindex = 0; jobindex < jobs.size(); jobindex++) {
-
-	
-			if((parameterMaxJobs != -1) && (parameterMaxJobs < (jobindex + 1))) {
-				System.out.println("maximum number of jobs, as specified, arrived. Stop further processing");
-				break;
-			}
-			if(parameterMaxMinutes != -1) {
-				Long tempprogramstarttime = programStart.getTime();
-				Long maxTime = programStart.getTime() + parameterMaxMinutes * 60 * 1000;
-				java.util.Date now = new java.util.Date();
-				if(now.getTime() > maxTime) {
-					System.out.println("maximum time, as specified, arrived. Stop further processing");
-					break;
-				} else {
-					System.out.println("Info: specified limit of minutes not arrived, useable minutes to work: " + Math.round((maxTime - now.getTime())/60/1000));
-				}
-			}
-
-			Job actjob = jobs.get(jobindex);
-
-			Long relationid = actjob.osmrelationid;
-			String jobname = actjob.jobname;
-
-			evaluation.setJobData(actjob);
-			
-			
-			jobstart = new java.util.Date();
-			System.out.println("start working on job " + actjob.toString() + "; started at " + jobstart.toString());
-
-			evaluation.setHousenumberAdditionCaseSensity(false);
-
-			java.util.Date dbloadstart = new java.util.Date();
-			list_housenumbers.clear();
-			//list_housenumbers = hnrreader.ReadListFromDB(evaluation);
-			list_housenumbers = hnrserver.ReadListFromServer(evaluation);
-			System.out.println("Number of official housenumbers: " + list_housenumbers.length());
-			if(list_housenumbers.length() == 0) {
-				System.out.println("Warning: job will be ignored, because no official housenumbers found for job " + actjob.toString() + "; started at " + jobstart.toString());
-				continue;
-			}
-			java.util.Date dbloadend = new java.util.Date();
-			java.util.Date overpassloadstart = new java.util.Date();
-			osm_housenumbers.clear();
-			osm_housenumbers = osmreader.ReadDataFromOverpass(evaluation, relationid);
-			System.out.println("Number of OSM housenumbers: " + osm_housenumbers.length());
-			//evaluation.osmtime will be set inside readdatafromoverpass
-			evaluation.evaluationtime = overpassloadstart.getTime();
-			java.util.Date overpassloadend = new java.util.Date();
-			java.util.Date mergedatastart = new java.util.Date();
-			evaluated_housenumbers.clear();
+			// local file, work offline (only connect to osm data via overpass
+		if(! parameterImportdateiname.equals("")) {
+			// client-server mode, connect to regio-osm.de Server and API and get jobs
+			list_housenumbers = hnrreader.ReadListFromFile(evaluation, parameterImportdateiname, parameterFieldSeparator, parameterHousenumbersCaseSensity);
+			osm_housenumbers = osmreader.ReadDataFromOverpass(evaluation, parameterOSMRelationid);
 			evaluated_housenumbers = list_housenumbers.merge(osm_housenumbers);
-			java.util.Date mergedataend = new java.util.Date();
 			evaluated_housenumbers.printhtml("test.html");
 			System.out.println("Number of housenumberlist entries after load of official housenumber list: "
 				+ evaluation.housenumberlist.length() + "   " + evaluation.housenumberlist.count_unchanged());
+		} else {
+	
+			HousenumberServerAPI hnrserver = new HousenumberServerAPI();
+	
+			hnrreader.setDBConnection(	configuration.db_application_url, 
+										configuration.db_application_username, 
+										configuration.db_application_password);
 
-			int lfdnr = 0;
-			java.util.Date uploadstart = null;
-			java.util.Date uploadend = null;
-			//while(lfdnr < 1000) {
-				lfdnr++;
-				System.out.println("upload Nr. " + lfdnr);
-				uploadstart = new java.util.Date();
-				hnrserver.writeEvaluationToServer(evaluation, evaluated_housenumbers);
-				uploadend = new java.util.Date();
-			//}
-			System.out.println("time for db load time in sek: " + (dbloadend.getTime() - dbloadstart.getTime())/1000);
-			System.out.println("time for overpass load time in sek: " + (overpassloadend.getTime() - overpassloadstart.getTime())/1000);
-			System.out.println("time for internal merge time in sek: " + (mergedataend.getTime() - mergedatastart.getTime())/1000);
-			System.out.println("time for result upload time in sek: " + (uploadend.getTime() - uploadstart.getTime())/1000);
+			List<Job> jobs;
+	
+			if(parameterAllMissingJobs)
+				jobs = hnrserver.getMissingCountryJobs(parameterCountry);
+			else
+				jobs = hnrserver.findJobs(parameterCountry,parameterMunicipiality, parameterJobname, parameterOfficialkeysId);
+			
+	
+			//jobs = hnrserver.findJobs("Schweiz","Zürich", "Zürich", "*");
+			
+			System.out.println("Number of Jobs received from Server: " + jobs.size());
+			
+			java.util.Date jobstart = new java.util.Date();
+			java.util.Date jobend = new java.util.Date();
+	
+			for(int jobindex = 0; jobindex < jobs.size(); jobindex++) {
+	
+		
+				if((parameterMaxJobs != -1) && (parameterMaxJobs < (jobindex + 1))) {
+					System.out.println("maximum number of jobs, as specified, arrived. Stop further processing");
+					break;
+				}
+				if(parameterMaxMinutes != -1) {
+					Long tempprogramstarttime = programStart.getTime();
+					Long maxTime = programStart.getTime() + parameterMaxMinutes * 60 * 1000;
+					java.util.Date now = new java.util.Date();
+					if(now.getTime() > maxTime) {
+						System.out.println("maximum time, as specified, arrived. Stop further processing");
+						break;
+					} else {
+						System.out.println("Info: specified limit of minutes not arrived, useable minutes to work: " + Math.round((maxTime - now.getTime())/60/1000));
+					}
+				}
+	
+				Job actjob = jobs.get(jobindex);
+	
+				Long relationid = actjob.osmrelationid;
+				String jobname = actjob.jobname;
+	
+				evaluation.setJobData(actjob);
+				
+				
+				jobstart = new java.util.Date();
+				System.out.println("start working on job " + actjob.toString() + "; started at " + jobstart.toString());
+	
+				evaluation.setHousenumberAdditionCaseSensity(false);
 
-			jobend = new java.util.Date();
-			System.out.println("finished working on job " + actjob.toString() + "; ended at " + jobend.toString() + " duration in sec: " 
-				+ (jobend.getTime() - jobstart.getTime())/1000);
-		}
+				list_housenumbers.clear();
+				osm_housenumbers.clear();
+				evaluated_housenumbers.clear();
+				
+				
+				java.util.Date dbloadstart = new java.util.Date();
+				list_housenumbers.clear();
+				//list_housenumbers = hnrreader.ReadListFromDB(evaluation);
+				list_housenumbers = hnrserver.ReadListFromServer(evaluation);
+				System.out.println("Number of official housenumbers: " + list_housenumbers.length());
+				if(list_housenumbers.length() == 0) {
+					System.out.println("Warning: job will be ignored, because no official housenumbers found for job " + actjob.toString() + "; started at " + jobstart.toString());
+					continue;
+				}
+				java.util.Date dbloadend = new java.util.Date();
+				java.util.Date overpassloadstart = new java.util.Date();
+				osm_housenumbers.clear();
+				osm_housenumbers = osmreader.ReadDataFromOverpass(evaluation, relationid);
+				System.out.println("Number of OSM housenumbers: " + osm_housenumbers.length());
+				//evaluation.osmtime will be set inside readdatafromoverpass
+				evaluation.evaluationtime = overpassloadstart.getTime();
+				java.util.Date overpassloadend = new java.util.Date();
+				java.util.Date mergedatastart = new java.util.Date();
+				evaluated_housenumbers.clear();
+				evaluated_housenumbers = list_housenumbers.merge(osm_housenumbers);
+				java.util.Date mergedataend = new java.util.Date();
+				evaluated_housenumbers.printhtml("test.html");
+				System.out.println("Number of housenumberlist entries after load of official housenumber list: "
+					+ evaluation.housenumberlist.length() + "   " + evaluation.housenumberlist.count_unchanged());
+	
+				int lfdnr = 0;
+				java.util.Date uploadstart = null;
+				java.util.Date uploadend = null;
+				//while(lfdnr < 1000) {
+					lfdnr++;
+					System.out.println("upload Nr. " + lfdnr);
+					uploadstart = new java.util.Date();
+					hnrserver.writeEvaluationToServer(evaluation, evaluated_housenumbers);
+					uploadend = new java.util.Date();
+				//}
+				System.out.println("time for db load time in sek: " + (dbloadend.getTime() - dbloadstart.getTime())/1000);
+				System.out.println("time for overpass load time in sek: " + (overpassloadend.getTime() - overpassloadstart.getTime())/1000);
+				System.out.println("time for internal merge time in sek: " + (mergedataend.getTime() - mergedatastart.getTime())/1000);
+				System.out.println("time for result upload time in sek: " + (uploadend.getTime() - uploadstart.getTime())/1000);
+	
+				jobend = new java.util.Date();
+				System.out.println("finished working on job " + actjob.toString() + "; ended at " + jobend.toString() + " duration in sec: " 
+					+ (jobend.getTime() - jobstart.getTime())/1000);
+			}
+		}	// end of else case (client/serer mode)
+
 		java.util.Date programEnd = new java.util.Date();
 		System.out.println("Program finished at " + programEnd.toString() + " duration in sec: "
 			+ (programEnd.getTime() - programStart.getTime())/1000);
